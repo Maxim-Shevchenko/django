925bbc5618fd8025142d9eefff103bd0e95b88be
Max Shevchenko <max19096@mail.ru>
2022-03-17 21:01:14 +0300

    добавление формы. формирование post - запрос на сервер по форме фитбэка

--- guest_book/config/reviews/views.py
+++ guest_book/config/reviews/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from reviews.forms import ReviewForm
 def reviews(request):
-    name_1 = "Вася"
-    email_1 = "vasya@example.com"
-    review_1 = "Все понравилось, обязательно приду еще!"
-    return render(request, 'reviews.html',{'name_1': name_1, 'email_1': email_1, 'review_1': review_1})
-# Create your views here.
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            name = data.get('name')
+            email = data.get('email')
+            review = data.get('review')
+            rating = data.get('rating')
+            with open('data.csv', 'a') as file:
+                file.write(f'{name}|{email}|{review}|{rating}\n')
+            return redirect('reviews')
+    else:
+        form = ReviewForm()
+        return render(request, 'reviews.html', {'form': form})
+
