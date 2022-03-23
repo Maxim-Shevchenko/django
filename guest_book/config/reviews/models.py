e5f8dbf81d135d56d4b357e7116c3b205432c64b
Max Shevchenko <max19096@mail.ru>
2022-03-23 15:52:39 +0300

    миграция данных. подготовка к подключение к бд

--- guest_book/config/reviews/models.py
+++ guest_book/config/reviews/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+class Review(models.Model):
+    name = models.CharField(max_length=25)
+    email = models.EmailField()
+    rating = models.IntegerField()
+    review = models.TextField()
 # Create your models here.
--- guest_book/config/reviews/views.py
+++ guest_book/config/reviews/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 from reviews.forms import ReviewForm
+from reviews.models import Review
 def reviews(request):
     if request.method == 'POST':
         form = ReviewForm(request.POST)
@@ -10,10 +11,29 @@ def reviews(request):
             email = data.get('email')
             review = data.get('review')
             rating = data.get('rating')
-            with open('data.csv', 'a') as file:
-                file.write(f'{name}|{email}|{review}|{rating}\n')
+            #with open('data.csv', 'a') as file:
+               # file.write(f'{name}|{email}|{review}|{rating}\n')
+            Review.objects.create(name=name, email=email, review=review, rating=rating)
             return redirect('reviews')
-    else:
-        form = ReviewForm()
-        return render(request, 'reviews.html', {'form': form})
+ #  else:
+ #      form = ReviewForm()
+ #      return render(request, 'reviews.html', {'form': form})
+
+  #  with open('data.csv') as file:
+    #   reviews = file.readlines()
+    #   if len(reviews) > 0:
+    #       review_data_1 = reviews[0]
+    #       name_1 = review_data_1.split('|')[0]
+    #       email_1 = review_data_1.split('|')[1]
+    #       review_1 = review_data_1.split('|')[2]
+    #   else:
+    #       name_1 = ""
+    #       email_1 = ""
+    #       review_1 = ""
+
+    form = ReviewForm()
+    reviews = Review.objects.all()
+
+    return render(request, 'reviews.html', {'form': form, 'reviews': reviews})
+    #return render(request, 'reviews.html', {'form': form, 'name_1': name_1, 'email_1': email_1, 'review_1': review_1})
 
